386b8ab39f7f1c4919d4efbabebde1c8844e7a0b
Sudhakar <usudhakar@informatica.com>
2019-07-03 16:26:23 +0530

    added multiple pages support

--- service/handler.go
+++ service/handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"regexp"
 
@@ -33,17 +34,23 @@ func getVin(res http.ResponseWriter, w *http.Request) {
 	var vin []string
 
 	url := w.URL.Query().Get("url")
-	resp, err := soup.Get(url)
-	if err != nil {
-		fmt.Println("error in fetching the data from url ")
-	}
-	//fmt.Println(resp)
-	r, _ := regexp.Compile("vin\":\"(\\w.+?)\"")
-	f := r.FindAllStringSubmatch(resp, -1)
-	for i := range f {
-		vin = append(vin, f[i][1])
+	pageLen, _ := strconv.Atoi(w.URL.Query().Get("pages"))
 
-	}
+	for j := 1; j <= pageLen; j++ {
 
+		newurl := url + "?page=" + strconv.Itoa(j)
+
+		resp, err := soup.Get(newurl)
+		if err != nil {
+			fmt.Println("error in fetching the data from url ")
+		}
+		//fmt.Println(resp)
+		r, _ := regexp.Compile("vin\":\"(\\w.+?)\"")
+		f := r.FindAllStringSubmatch(resp, -1)
+		for i := range f {
+			vin = append(vin, f[i][1])
+
+		}
+	}
 	RespondWithJSON(vin, res, w)
 }
